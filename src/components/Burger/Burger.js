f053d8c3701984486685d3eaeec3bf50081585af
mozieblo <m.ozieblo@wp.pl>
2019-11-05 19:54:27 +0100

    update

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import BurgerIngredient from '../Burger/BurgerIngredient/BurgerIngredient';
+import classes from './Burger.module.css';
+
+const burger = ( props ) => {
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top"/>
+            <BurgerIngredient type="salad"/>
+            <BurgerIngredient type="cheese"/>
+            <BurgerIngredient type="meat"/>
+            <BurgerIngredient type="bacon"/>
+            <BurgerIngredient type="bread-bottom"/>
+        </div>
+    );
+}
+
+export default burger;
