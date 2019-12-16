c4202344e6f97ab0b1f672ada450577e898234a0
mozieblo <m.ozieblo@wp.pl>
2019-12-16 15:51:08 +0100

    poprawka

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -27,7 +27,6 @@ export const purchaseBurger = ( orderData ) => {
         dispatch(purchaseBurgerStart());
         axios.post('orders.json', orderData)
         .then(response => {
-            console.log(response.data)
             dispatch(purchaseBurgerSuccess(response.data.name, orderData))
         })
         .catch(error => {
