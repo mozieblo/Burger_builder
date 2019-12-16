56f8f7fa9b3bcafadb9a64b74472bf1cd202eb22
mozieblo <m.ozieblo@wp.pl>
2019-12-16 13:27:09 +0100

    goodOrder

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -5,9 +5,9 @@ import BuildControl from './BuildControl/BuildControl';
 
 const controls = [
     { label: 'Salad', type: 'salad' },
-    { label: 'Meat', type: 'meat' },
-    { label: 'Cheese', type: 'cheese' },
     { label: 'Bacon', type: 'bacon' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' },
 ]
 
 const buildControls = ( props ) => (
